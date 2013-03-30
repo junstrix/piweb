11a6a096ef2759313188b2b0975efa25c1ecd99a
junstrix <junstrix@gmail.com>
2013-03-30 22:42:46 +0800

    init project

---
+++ pi_dev/urls.py
@@ -0,0 +1,24 @@
+# from django.conf.urls import patterns, include, url
+from django.conf.urls import *
+from pi_dev.views import Pi_Index
+from pi_dev.views import hello
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'pi_dev.views.home', name='home'),
+    # url(r'^pi_dev/', include('pi_dev.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    (r'^$',Pi_Index),
+    ('^pi/$', Pi_Index),
+    ('^hello/$', hello),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve'),
+)
---
+++ pi_dev/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+import datetime
+import commands
+
+def Pi_Index(request):
+    now = datetime.datetime.now()
+    osuptime = commands.getoutput('uptime').split(',')[0]
+    sosuptime = commands.getoutput('uptime')
+    oscpuinfo = commands.getoutput('cat /proc/cpuinfo')
+    sosfree = commands.getoutput('free -m')
+    soslogo = commands.getoutput('linuxlogo -a -u')
+    return render_to_response('pi/pi.html', {'current_date': now,'os_uptime': osuptime,'os_cpuinfo': oscpuinfo,'sos_uptime': sosuptime,'sys_free':sosfree,'sys_linuxlogo': soslogo})
+
+def hello(request):
+    return HttpResponse("Hello world")
