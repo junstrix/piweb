631b97f26c7468bb43fe0879b6fda720fecbe0f2
junstrix <junstrix@gmail.com>
2013-04-05 21:36:00 +0800

    add control login and session

--- control/views.py
+++ control/views.py
@@ -1,10 +1,31 @@
+#coding=utf-8
 # Create your views here.
-
+from django.contrib import auth
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("hello,world")
+    led_center = "RasPi Control Center"
+    current_time = datetime.today()
+    return render_to_response('con/led.html',{'led_center':led_center,
+        'current_time':current_time})
+def login_view(request):
+    """user login page"""
+    username = request.POST.get('username','')
+    password = request.POST.get('password','')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return HttpResponseRedirect('/control/control/')
+    else:
+        return HttpResponseRedirect('/control/invalid/')
+
+def logout_view(request):
+    """user logout page"""
+    auth.logout(request)
+    return HttpResponseRedirect('/control/logedout/')
+
 def tform(request):
     if request.POST.has_key('uname'):
         uname = request.POST.get('uname')
@@ -12,19 +33,27 @@ def tform(request):
         return HttpResponse('you has logged')
     else:
         return render_to_response('con/test.html')
+
 def search_form(request):
     """search-form get method test"""
     return render_to_response('con/search_form.html')
+
 def search(request):
     if 'q' in request.GET:
         message = 'You search for : %r' % request.GET['q']
     else:
         message = 'you submitted an empty form.'
     return HttpResponse(message)
-def led(request):
-    """led control"""
-    if 'on' in request.GET:
-        """some control code"""
-        return HttpResponse('led already turned on yeah')
-    else:
-        return render_to_response('con/led.html')
+
+def login(request):
+    led_center = "用户登录"
+    current_time = datetime.today()
+    return render_to_response('con/login.html',{'led_center':led_center,'current_time':current_time})
+
+def control(request):
+    """get a post control gpio"""
+    if not request.user.is_authenticated():
+        return HttpResponse("你无权访问...")
+    led_center="raspberry pi control"
+    current_time = datetime.today()
+    return render_to_response('con/control.html',{'led_center':led_center,'current_time':current_time})
